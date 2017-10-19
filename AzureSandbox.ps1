f3bf835c1a2afae5cbe1437049e29ae1d6d9562d
otti <othmar.kesseli@outlook.com>
2017-10-19 18:12:25 +0200

    InitVersion

---
+++ AzureSandbox.ps1
@@ -0,0 +1,58 @@
+import-module AzureRM
+
+$pass = ConvertTo-SecureString "W80RJe8mqPuITJLt2qz/roEATJ9BxdCo8TSoxjSurWQ=" -AsPlainText –Force
+$cred = New-Object -TypeName pscredential –ArgumentList "4b3273ca-d65c-47bc-979a-9fd70c03fc44", $pass
+Login-AzureRmAccount -Credential $cred -ServicePrincipal -TenantId 38e69ad1-2007-434c-880e-4f9c1c98ac4b -SubscriptionId 9bc20da0-8454-4a4e-ae08-ada2180eb46e
+
+# Variables    
+## Global
+$ResourceGroupName = "sampleSme"
+$Location = "NorthEurope"
+
+## Storage
+$StorageName = "samplesmestorage"
+$StorageType = "Standard_GRS"
+
+## Network
+$InterfaceName = "ServerInterface06"
+$Subnet1Name = "Subnet1"
+$VNetName = "VNet09"
+$VNetAddressPrefix = "10.0.0.0/16"
+$VNetSubnetAddressPrefix = "10.0.0.0/24"
+
+## Compute
+$VMName = "VirtualMachine12"
+$ComputerName = "Server22"
+$VMSize = "Standard_A2"
+$OSDiskName = $VMName + "OSDisk"
+
+# Resource Group
+New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location
+
+# Storage
+$StorageAccount = New-AzureRmStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageName -Type $StorageType -Location $Location
+
+# Network
+$PIp = New-AzureRmPublicIpAddress -Name $InterfaceName -ResourceGroupName $ResourceGroupName -Location $Location -AllocationMethod Dynamic
+$SubnetConfig = New-AzureRmVirtualNetworkSubnetConfig -Name $Subnet1Name -AddressPrefix $VNetSubnetAddressPrefix
+$VNet = New-AzureRmVirtualNetwork -Name $VNetName -ResourceGroupName $ResourceGroupName -Location $Location -AddressPrefix $VNetAddressPrefix -Subnet $SubnetConfig
+$Interface = New-AzureRmNetworkInterface -Name $InterfaceName -ResourceGroupName $ResourceGroupName -Location $Location -SubnetId $VNet.Subnets[0].Id -PublicIpAddressId $PIp.Id
+
+# Compute
+
+## Setup local VM object
+$username = "smeAdmin"
+$password = "1234%%abcd" | convertto-securestring
+$Credential = new-object -typename System.Management.Automation.PSCredential `
+         -argumentlist $username, $password
+
+$VirtualMachine = New-AzureRmVMConfig -VMName $VMName -VMSize $VMSize
+$VirtualMachine = Set-AzureRmVMOperatingSystem -VM $VirtualMachine -Windows -ComputerName $ComputerName -Credential $Credential -ProvisionVMAgent -EnableAutoUpdate
+$VirtualMachine = Set-AzureRmVMSourceImage -VM $VirtualMachine -PublisherName MicrosoftWindowsServer -Offer WindowsServer -Skus 2016-Datacenter -Version "latest"
+$VirtualMachine = Add-AzureRmVMNetworkInterface -VM $VirtualMachine -Id $Interface.Id
+$OSDiskUri = $StorageAccount.PrimaryEndpoints.Blob.ToString() + "vhds/" + $OSDiskName + ".vhd"
+$VirtualMachine = Set-AzureRmVMOSDisk -VM $VirtualMachine -Name $OSDiskName -VhdUri $OSDiskUri -CreateOption FromImage
+
+## Create the VM in Azure
+$newVM = New-AzureRmVM -ResourceGroupName $ResourceGroupName -Location $Location -VM $VirtualMachine
+$newVM
---
+++ TestRbKoE.ps1
@@ -0,0 +1,8 @@
+write-host "Test KoE"
+
+# $uri = "https://login.microsoftonline.com/38e69ad1-2007-434c-880e-4f9c1c98ac4b/oauth2/token?api-version=1.0"
+# $body = "grant_type=client_credentials&resource=https%3A%2F%2Fmanagement.core.windows.net%2F&client_id=3c58ef7b-3e46-4c98-9664-b9da1dc37082&client_secret=o1QcHmHAKCIFe1GhXwnZBgkIdNSGGzq0fYpD3YHdvHM="
+# $headers = @{"content-type"="application/x-www-form-urlencoded"}
+# $result = Invoke-WebRequest -Uri $uri -Body $body -Headers $headers -Method "POST"
+
+# $result
